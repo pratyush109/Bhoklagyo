d9d0716ad8a3e2923787837cee0a204ffa84c615
Pratyush Adhikari <pratyushadhikari785@gmail.com>
2026-01-31 15:34:57 +0545

    added the splash activity

--- app/src/main/java/com/example/bhoklagyo/SplashActivity.kt
+++ app/src/main/java/com/example/bhoklagyo/SplashActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.bhoklagyo.DashboardActivity
 import com.example.bhoklagyo.ui.theme.Primary
 import com.example.bhoklagyo.ui.theme.White
 import com.example.bhoklagyo.viewmodel.AuthViewModel
